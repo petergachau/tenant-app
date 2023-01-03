9719184b13f8dbf79b5ade6598e995cacdeb6a01
petergachau <83207831+petergachau@users.noreply.github.com>
2023-01-03 18:45:48 +0300

    Update index.js
     h

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-
 app.use("/users", userRouter); // http://localhost:5000/users/signup
 app.use("/project", tourRouter);
 app.use('/milestone', milestone)
