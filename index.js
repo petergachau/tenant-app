c83ad24d291d7ee548ec4b3187c99e6b2dc7a2b9
petergachau <83207831+petergachau@users.noreply.github.com>
2023-01-03 17:00:30 +0300

    j

--- index.js
+++ index.js
@@ -31,11 +31,6 @@ import messageRouter from './routes/message.js'
 const PORT=5000;
 const app = express();
 
-//twilio requirements -- Texting API .js
-// const accountSid = '___YOUR___ACCOUNT__SID';
-// const authToken = '___YOUR___AUTHENTICATION__TOKEN'; 
-// const client = new twilio(accountSid, authToken);
-
 dotenv.config();
 
 app.use(express.json({ limit: "4mb" }));
