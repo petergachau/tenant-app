c5f01b51da107dd94e76cb98f990fbaedf5c091e
petergachau <83207831+petergachau@users.noreply.github.com>
2023-01-03 17:00:54 +0300

    h

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ import singleSummaryRouter  from './routes/summarySingles.js'
 import mpesaRouter from './routes/mpesa.js'
 import messageRouter from './routes/message.js'
 
-const PORT=5000;
-const app = express();
 
-dotenv.config();
+dotenv.config()
+
+
+const app=express()
+const PORT=5000;
 
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
